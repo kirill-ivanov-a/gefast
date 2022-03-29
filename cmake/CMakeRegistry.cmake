6030d919394ac94ec1ed0db0452a8215e2eb7b48
Kirill Ivanov <ivanov.kirill.a@mail.ru>
2022-03-29 19:49:52 +0300

    Installation fixed

--- cmake/CMakeRegistry.cmake
+++ cmake/CMakeRegistry.cmake
@@ -9,21 +9,21 @@
 #     -DCMAKE_REGISTRY_FOLDER="INSTALL_FOLDER"
 #     -DCMAKE_REGISTRY_FOLDER="BUILD_FOLDER"
 #
-if( NOT (DEFINED CMAKE_REGISTRY_FOLDER) )
-  set(CMAKE_REGISTRY_FOLDER "OFF"
-    CACHE STRING "Choose CMake registry folder." FORCE)
-endif()
+if (NOT (DEFINED CMAKE_REGISTRY_FOLDER))
+    set(CMAKE_REGISTRY_FOLDER "OFF"
+            CACHE STRING "Choose CMake registry folder." FORCE)
+endif ()
 
 # Set possible values for cmake-gui
 set_property(CACHE CMAKE_REGISTRY_FOLDER PROPERTY STRINGS
-  "BUILD_FOLDER" "INSTALL_FOLDER" "OFF")
+        "BUILD_FOLDER" "INSTALL_FOLDER" "OFF")
 message(STATUS "CMAKE_REGISTRY_FOLDER: ${CMAKE_REGISTRY_FOLDER}")
 
 
 # CMake Register (build directory)
-if(CMAKE_REGISTRY_FOLDER STREQUAL "BUILD_FOLDER")
-  export(PACKAGE ${PROJECT_NAME})
-endif()
+if (CMAKE_REGISTRY_FOLDER STREQUAL "BUILD_FOLDER")
+    export(PACKAGE ${PROJECT_NAME})
+endif ()
 
 #
 # Register installed package with CMake
@@ -34,16 +34,16 @@ endif()
 # installation prefix. This differs from CMake's export(PACKAGE) command
 # which registers the build directory instead.
 #
-function (register_package PACKAGE_FOLDER)
-  if (NOT IS_ABSOLUTE "${PACKAGE_FOLDER}")
-    set (PACKAGE_FOLDER "${CMAKE_INSTALL_PREFIX}/${PACKAGE_FOLDER}")
-  endif ()
+function(register_package PACKAGE_FOLDER)
+    if (NOT IS_ABSOLUTE "${PACKAGE_FOLDER}")
+        set(PACKAGE_FOLDER "${CMAKE_INSTALL_PREFIX}/${PACKAGE_FOLDER}")
+    endif ()
 
-  string (MD5 REGISTRY_ENTRY "${PACKAGE_FOLDER}")
+    string(MD5 REGISTRY_ENTRY "${PACKAGE_FOLDER}")
 
-  if (WIN32)
-    install (CODE
-      "execute_process (
+    if (WIN32)
+        install(CODE
+                "execute_process (
          COMMAND reg add \"HKCU\\\\Software\\\\Kitware\\\\CMake\\\\Packages\\\\${PROJECT_NAME}\" /v \"${REGISTRY_ENTRY}\" /d \"${PACKAGE_FOLDER}\" /t REG_SZ /f
          RESULT_VARIABLE RT
          ERROR_VARIABLE  ERR
@@ -55,19 +55,19 @@ function (register_package PACKAGE_FOLDER)
          string (STRIP \"\${ERR}\" ERR)
          message (STATUS \"Register:   Failed to add registry entry: \${ERR}\")
        endif ()"
-    )
-  elseif (IS_DIRECTORY "$ENV{HOME}")
-    file (WRITE "${PROJECT_BINARY_DIR}/${PROJECT_NAME}-registry-entry" "${PACKAGE_FOLDER}")
-    install (
-      FILES       "${PROJECT_BINARY_DIR}/${PROJECT_NAME}-registry-entry"
-      DESTINATION "$ENV{HOME}/.cmake/packages/${PROJECT_NAME}"
-      RENAME      "${REGISTRY_ENTRY}"
-    )
-    message (STATUS "CMake registry: $ENV{HOME}/.cmake/packages/${PROJECT_NAME}")
-  endif ()
-endfunction ()
+                )
+    elseif (IS_DIRECTORY "$ENV{HOME}")
+        file(WRITE "${PROJECT_BINARY_DIR}/${PROJECT_NAME}-registry-entry" "${PACKAGE_FOLDER}")
+        install(
+                FILES "${PROJECT_BINARY_DIR}/${PROJECT_NAME}-registry-entry"
+                DESTINATION "$ENV{HOME}/.cmake/packages/${PROJECT_NAME}"
+                RENAME "${REGISTRY_ENTRY}"
+        )
+        message(STATUS "CMake registry: $ENV{HOME}/.cmake/packages/${PROJECT_NAME}")
+    endif ()
+endfunction()
 
 # CMake Register (install directory)
-if(CMAKE_REGISTRY_FOLDER STREQUAL "INSTALL_FOLDER")
-  register_package(${CONFIG_INSTALL_DIR})
+if (CMAKE_REGISTRY_FOLDER STREQUAL "INSTALL_FOLDER")
+    register_package(${CONFIG_INSTALL_DIR})
 endif ()
--- cmake/SetEnv.cmake
+++ cmake/SetEnv.cmake
@@ -3,15 +3,15 @@ string(TOUPPER ${PROJECT_NAME} PROJECT_NAME_UPPERCASE)
 string(TOLOWER ${PROJECT_NAME} PROJECT_NAME_LOWERCASE)
 
 # Library name (by default is the project name)
-if(NOT LIBRARY_NAME)
-  set(LIBRARY_NAME ${PROJECT_NAME_LOWERCASE})
-endif()
+if (NOT LIBRARY_NAME)
+    set(LIBRARY_NAME ${PROJECT_NAME_LOWERCASE})
+endif ()
 
 # Library folder name (by default is the project name in lowercase)
 # Example: #include <foo/foo.h>
-if(NOT LIBRARY_FOLDER)
-  set(LIBRARY_FOLDER ${PROJECT_NAME_LOWERCASE})
-endif()
+if (NOT LIBRARY_FOLDER)
+    set(LIBRARY_FOLDER ${PROJECT_NAME_LOWERCASE})
+endif ()
 
 # Make sure different configurations don't collide
 set(CMAKE_DEBUG_POSTFIX "d")
@@ -26,37 +26,37 @@ message(STATUS "BUILD_SHARED_LIBS: ${BUILD_SHARED_LIBS}")
 # Also no reason of using CMAKE_BUILD_TYPE if it's a multiconfig generator.
 #
 get_property(isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
-if(isMultiConfig)
-  set(CMAKE_CONFIGURATION_TYPES
-      "Release;Debug;MinSizeRel;RelWithDebInfo" CACHE STRING "" FORCE)
+if (isMultiConfig)
+    set(CMAKE_CONFIGURATION_TYPES
+            "Release;Debug;MinSizeRel;RelWithDebInfo" CACHE STRING "" FORCE)
 
-  message(STATUS "CMAKE_CONFIGURATION_TYPES: ${CMAKE_CONFIGURATION_TYPES}")
-  message(STATUS "CMAKE_GENERATOR: Multi-config")
-else()
-  # Set a default build type if none was specified
-  if(NOT CMAKE_BUILD_TYPE)
-    set(CMAKE_BUILD_TYPE Release CACHE STRING "Choose the type of build." FORCE)
-  endif()
+    message(STATUS "CMAKE_CONFIGURATION_TYPES: ${CMAKE_CONFIGURATION_TYPES}")
+    message(STATUS "CMAKE_GENERATOR: Multi-config")
+else ()
+    # Set a default build type if none was specified
+    if (NOT CMAKE_BUILD_TYPE)
+        set(CMAKE_BUILD_TYPE Release CACHE STRING "Choose the type of build." FORCE)
+    endif ()
 
-  # Set the possible values of build type for cmake-gui
-  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
-               "Release" "Debug" "MinSizeRel" "RelWithDebInfo")
+    # Set the possible values of build type for cmake-gui
+    set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
+            "Release" "Debug" "MinSizeRel" "RelWithDebInfo")
 
-  message(STATUS "CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE}")
-  message(STATUS "CMAKE_GENERATOR: Single-config")
-endif()
+    message(STATUS "CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE}")
+    message(STATUS "CMAKE_GENERATOR: Single-config")
+endif ()
 message(STATUS "CMAKE_GENERATOR: ${CMAKE_GENERATOR}")
 
 # Generated headers folder
 set(GENERATED_HEADERS_DIR
-  "${CMAKE_CURRENT_BINARY_DIR}/generated_headers"
-)
+        "${CMAKE_CURRENT_BINARY_DIR}/generated_headers"
+        )
 
 # Create 'version.h'
 configure_file(
-  "${PROJECT_SOURCE_DIR}/${LIBRARY_FOLDER}/version.h.in"
-  "${GENERATED_HEADERS_DIR}/${LIBRARY_FOLDER}/version.h"
-  @ONLY
+        "${PROJECT_SOURCE_DIR}/${LIBRARY_FOLDER}/version.h.in"
+        "${GENERATED_HEADERS_DIR}/${LIBRARY_FOLDER}/version.h"
+        @ONLY
 )
 
 # Introduce variables:
@@ -72,7 +72,7 @@ include(GNUInstallDirs)
 set(CONFIG_INSTALL_DIR "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}")
 
 # Configuration
-set(GENERATED_DIR       "${CMAKE_CURRENT_BINARY_DIR}/generated")
+set(GENERATED_DIR "${CMAKE_CURRENT_BINARY_DIR}/generated")
 set(VERSION_CONFIG_FILE "${GENERATED_DIR}/${PROJECT_NAME}ConfigVersion.cmake")
 set(PROJECT_CONFIG_FILE "${GENERATED_DIR}/${PROJECT_NAME}Config.cmake")
 set(TARGETS_EXPORT_NAME "${PROJECT_NAME}Targets")
@@ -86,9 +86,9 @@ include(CMakePackageConfigHelpers)
 # Use:
 #   - PROJECT_VERSION
 write_basic_package_version_file(
-    "${VERSION_CONFIG_FILE}"
-    VERSION "${${PROJECT_NAME}_VERSION}"
-    COMPATIBILITY SameMajorVersion
+        "${VERSION_CONFIG_FILE}"
+        VERSION "${${PROJECT_NAME}_VERSION}"
+        COMPATIBILITY SameMajorVersion
 )
 
 # Configure '<PROJECT-NAME>Config.cmake'
@@ -96,40 +96,40 @@ write_basic_package_version_file(
 #   - TARGETS_EXPORT_NAME
 #   - PROJECT_NAME
 configure_package_config_file(
-    "${PROJECT_SOURCE_DIR}/cmake/Config.cmake.in"
-    "${PROJECT_CONFIG_FILE}"
-      INSTALL_DESTINATION "${CONFIG_INSTALL_DIR}"
+        "${PROJECT_SOURCE_DIR}/cmake/Config.cmake.in"
+        "${PROJECT_CONFIG_FILE}"
+        INSTALL_DESTINATION "${CONFIG_INSTALL_DIR}"
 )
 
 # Uninstall targets
 configure_file("${PROJECT_SOURCE_DIR}/cmake/Uninstall.cmake.in"
-  "${GENERATED_DIR}/Uninstall.cmake"
-  IMMEDIATE @ONLY)
+        "${GENERATED_DIR}/Uninstall.cmake"
+        IMMEDIATE @ONLY)
 add_custom_target(uninstall
-  COMMAND ${CMAKE_COMMAND} -P ${GENERATED_DIR}/Uninstall.cmake)
+        COMMAND ${CMAKE_COMMAND} -P ${GENERATED_DIR}/Uninstall.cmake)
 
 # Always full RPATH (for shared libraries)
 #  https://gitlab.kitware.com/cmake/community/-/wikis/doc/cmake/RPATH-handling
-if(BUILD_SHARED_LIBS)
-  # use, i.e. don't skip the full RPATH for the build tree
-  set(CMAKE_SKIP_BUILD_RPATH FALSE)
+if (BUILD_SHARED_LIBS)
+    # use, i.e. don't skip the full RPATH for the build tree
+    set(CMAKE_SKIP_BUILD_RPATH FALSE)
 
-  # when building, don't use the install RPATH already
-  # (but later on when installing)
-  set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
+    # when building, don't use the install RPATH already
+    # (but later on when installing)
+    set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
 
-  set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+    set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
 
-  # add the automatically determined parts of the RPATH
-  # which point to directories outside the build tree to the install RPATH
-  set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+    # add the automatically determined parts of the RPATH
+    # which point to directories outside the build tree to the install RPATH
+    set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
 
-  # the RPATH to be used when installing, but only if it's not a system directory
-  list(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib" isSystemDir)
-  if("${isSystemDir}" STREQUAL "-1")
-      set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
-  endif()
-endif()
+    # the RPATH to be used when installing, but only if it's not a system directory
+    list(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib" isSystemDir)
+    if ("${isSystemDir}" STREQUAL "-1")
+        set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+    endif ()
+endif ()
 
 # CMake Registry
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/CMakeRegistry.cmake)
